b6a5597813212098ef307300239028265aacfa2f
triemaryanto <triemaryanto93@gmail.com>
2023-11-27 18:36:21 +0700

    dev

--- resources/views/livewire/admin/pages/user.blade.php
+++ resources/views/livewire/admin/pages/user.blade.php
@@ -1,3 +1,10 @@
 <div>
-    {{-- Be like water. --}}
+    <x-slot name="header">
+        <livewire:admin.global.page-header judul="Dashboard" subjudul="Dasboard" :breadcrumb="[]" />
+    </x-slot>
+    <div class="card">
+        <div class="card-body">
+            <livewire:admin.table.user-list />
+        </div>
+    </div>
 </div>
