d1ea999af1d534dc1c0c88ad15bd49afdf6556c0
triemaryanto <triemaryanto93@gmail.com>
2023-11-27 15:31:32 +0700

    dev

---
+++ app/Livewire/Admin/Table/UserList.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire\Admin\Table;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\User;
+
+class UserList extends DataTableComponent
+{
+    protected $model = User::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Name", "name")
+                ->sortable()->searchable(),
+            Column::make("Email", "email")
+                ->sortable(),
+            Column::make("Created at", "created_at")
+                ->sortable(),
+            Column::make("Updated at", "updated_at")
+                ->sortable(),
+        ];
+    }
+}
