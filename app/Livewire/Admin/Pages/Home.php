d76cc820578ac009d955bb5aa5a3d08179723268
mnurcholis <chollez69@gmail.com>
2023-12-20 11:33:12 +0700

    update

--- app/Livewire/Admin/Pages/Home.php
+++ app/Livewire/Admin/Pages/Home.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\Admin\Pages;
 
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Livewire\Component;
 
 class Home extends Component
 {
+    public $users, $product, $order;
+
+    public function mount()
+    {
+        $this->users = User::whereHas('roles', function ($query) {
+            $query->where('name', 'user');
+        })->get();
+        $this->product = Product::get();
+        $this->order = Order::whereNotIn('payment_status', [3, 4])->get();
+    }
+
     public function render()
     {
         return view('livewire.admin.pages.home');
